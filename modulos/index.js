6df878a3c81b7f3a0ec8509c21e07ff7584aa886
jose luis <joseluiscor2015@gmail.com>
2023-07-13 21:04:11 -0500

    decisiones

--- modulos/index.js
+++ modulos/index.js
@@ -1,15 +1,48 @@
-import productos from "./productos.js";
-const borrar = document.getElementById("borrar")
-const formulario = document.getElementById("datos");
-formulario["guard"].addEventListener("click",(ev) => {
-    ev.preventDefault();
-    const obj = {
-        "nombre":formulario.nombre.value,
-        "edad":parseInt(formulario.edad.value)
-    }
-    productos.postProduct(obj);
+import rutas from "./rutas.js";
+const borrar       = document.getElementById("borrar");
+const elementRutas = document.getElementById("rutas");
+const formulario   = document.getElementById("GuardarR");
+const select       = document.getElementById("rutas");
+const puntos       = document.getElementById("puntos");
+
+
+select.addEventListener("change",async (ev) => {
+    puntos.innerHTML = ""
+    const id         = parseInt( ev.target.value ) 
+    const valores    = await rutas.getRelateTable(id);
+
+    valores.forEach(e => {
+        puntos.innerHTML += `
+        <div class = "col-md-4 ">
+            <div class = "card">
+                <div class="card-body">
+                    <h4 class= "card-head">${e.NomPuntos}</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            
+        </div>
+        
+        `;
+    })
 })
 
-borrar.addEventListener("click", () => {
-    productos.deleteProduct(7);
+window.onload = async function(){
+    const rutasDatos       = await rutas.getRutas();
+    elementRutas.innerHTML = "";
+    rutasDatos.forEach(element => {
+        elementRutas.innerHTML += `
+            <option value = "${element.id}" onclick= "buscar(this)">${element.NomRuta}</option>`; 
+    })
+}
+
+formulario.addEventListener("submit",async (ev) => {
+    ev.preventDefault();
+    console.log(formulario.nombreRuta.value);
+    const obj = {
+        "NomRuta":formulario.nombreRuta.value,
+    }
+    await rutas.postRutas(obj);
 })
+
---
+++ modulos/rutas.js
@@ -0,0 +1,46 @@
+import Modelo from './modelo.js';
+class Rutas extends Modelo{
+    constructor(){
+        super();
+        this.api = "Ruta";
+        this.relation = "Puntos"
+    }
+    async getRutas(){
+        try{
+            const req = await fetch(`${this.http}${this.api}`);
+            const datos = await req.json();
+            return datos;
+        }catch(err){
+            console.log(err);
+        }
+    }
+    async postRutas(body){
+        try{
+            const req = await fetch(`${this.http}${this.api}`, this.config("POST", body));
+            const datos = await req.json();
+            return datos;
+        }catch(err){
+            console.log(err);
+        }
+    }
+    async deleteRutas(id){
+        try{
+            const req = await fetch(`${this.http}${this.api}/${id}`, this.config("DELETE", id));
+            const datos = await req.json();
+            return datos;
+        }catch(err){
+            console.log(err);
+        }
+    }
+    async getRelateTable(id){
+        try{
+            const req = await fetch(`${this.http}${this.api}/${id}/${this.relation}`, this.config("GET", id));
+            const datos = await req.json();
+            return datos;
+        }catch(err){
+            console.log(err);
+        }
+    }
+}
+const rutas = new Rutas();
+export default rutas;
